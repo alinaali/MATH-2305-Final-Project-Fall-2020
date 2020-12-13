82072d12f9e765eaa8c011f9501e6e5ec0686837
Alina Ali <33349452+alinaali@users.noreply.github.com>
2020-12-13 13:53:46 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+'''
+import networkx as nx
+from functions import * 
+from drawing import *
+from algorithms import *
+'''
+import networkx as nx
+from functions import * 
+from drawing import *
+from algorithms import *
+
+
+graph_data = open('test-graphs/G1.txt','r') #read in the graph you want to test G1-G4
+'''
+will open and return a file object from test-graphs folder in this case a graph to be read,
+if file is not found will raise an error
+'''
+G = nx.read_weighted_edgelist(graph_data, nodetype = int) #create initial graph 
+'''
+NetworkX will then generate a plot for the chosen file
+and will ensure returning a hashable type
+'''
+T = prims_algorithm(G, int(input("Type starting vertex: ")), True, True) #ask the user what verticy they want to start on
+'''
+Will then ask for an input for a starting vertex 
+'''
 
